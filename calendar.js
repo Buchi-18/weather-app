2cec070bfdadedc7f724c3451cd9012828117dde
Buchi-18 <3690@gmail.com>
2023-03-14 17:19:26 +0900

    weather app frame work fixed

--- calendar.js
+++ calendar.js
@@ -1,15 +1,24 @@
 "use strict";
+import { honshu, hokkaido, okinawa } from "./weatherData.js";
+
+//
+// *************** ロケーション要素の取得 ***************
+const locationWrap = document.getElementById("locationWrap");
+const selectLocation = document.getElementById("selectLocation");
+export const currentLocation = document.getElementById("currentLocation");
+//
+// *************** カレンダー要素の取得 ***************
+const calendarWrap = document.getElementById("calendarWrap");
+const selectDate = document.getElementById("selectDate");
 const monthNam = document.getElementById("monthNam");
-const currentDate = document.querySelector(".current-date");
+export const currentDate = document.getElementById("currentDate");
 const calendarDates = document.querySelector(".dates ul");
-const selectDates = document.querySelector(".dates ul li");
-const selectable = document.querySelector(".selectable");
 const prevMonth = document.querySelector(".prev");
 const nextMonth = document.querySelector(".next");
-
+//
+// *************** 日付情報の取得 ***************
 const dt = new Date();
 dt.setFullYear(2023);
-
 let year = dt.getFullYear();
 let month = dt.getMonth();
 let date = dt.getDate();
@@ -19,19 +28,158 @@ let dayOfWeek = dayOfWeekStr[DOW];
 let dayOfWeekFirst = new Date(year, month, 1);
 let numberOfDays = new Date(year, month + 1, 0).getDate();
 let renderMonth = month + 1;
+//
+// *************** ウェザー要素の取得 ***************
+const weatherResult = document.getElementById("weatherResult");
+const weatherLocation = document.querySelector("#weatherResult .location");
+const weatherTitle = document.querySelector("#weatherResult h2");
+const resultBtn = document.getElementById("resultBtn");
+
+let userLocation;
+let userDate;
+const weatherDataYText = {
+  "01d": "せいてん",
+  "02d": "はれ",
+  "03d": "はれときどきくもり",
+  "04d": "くもり",
+  "09d": "こさめ",
+  "10d": "あめ",
+  "11d": "らいう",
+  "13d": "ゆき",
+  "50d": "きり",
+};
+//
+// *************** レンダリング処理 ***************
 render();
+renderWeather();
+
+//
+// *************** イベントハンドラー ***************
+currentLocation.addEventListener("click", function () {
+  //ロケーションアコーディオンハンドラー
+  AccordionHandler(locationWrap, selectLocation);
+});
+
+currentDate.addEventListener("click", function () {
+  //カレンダーアコーディオンハンドラー
+  AccordionHandler(calendarWrap, selectDate);
+});
 
 window.addEventListener("click", function (e) {
-  const isClick = e.target.className === "selectable";
-  if (!isClick) return;
-  const dateInfoArray = e.target.dataset.date.split("_");
-  renderMonth = parseFloat(dateInfoArray[1]);
-  date = parseFloat(dateInfoArray[2]);
-  dayOfWeek = dayOfWeekStr[parseFloat(dateInfoArray[3])];
-  currentDate.innerHTML = `<p>${renderMonth}/${date}(${dayOfWeek})</p>`;
+  //ロケーション選択
+  selectedLocation(e);
+});
+
+window.addEventListener("click", function (e) {
+  //日付選択
+  selectedDate(e);
 });
 
 prevMonth.addEventListener("click", function () {
+  // prevボタン １ヶ月前
+  subtractionMonth();
+});
+nextMonth.addEventListener("click", function () {
+  // nextボタン １ヶ月後
+  additionMonth();
+});
+
+resultBtn.addEventListener("click", function () {
+  renderWeather();
+  selectLocation.style.height = 0;
+  selectDate.style.height = 0;
+  locationWrap.classList.remove("active");
+  calendarWrap.classList.remove("active");
+});
+
+//
+// *************** メソッド ***************
+function render() {
+  let dateNum = 0;
+  let dayOfWeekNum = dayOfWeekFirst.getDay();
+
+  renderMonth = month + 1;
+  monthNam.innerHTML = renderMonth;
+  if (numberOfDays < date) {
+    date = date - (date - numberOfDays);
+  }
+
+  document.querySelector(".app-title span").innerHTML = `${year}年`;
+  currentDate.innerHTML = `<p data-date="${year}_${renderMonth}_${date}">${renderMonth}/${date}(${dayOfWeek})</p>`;
+
+  for (let i = 0; i <= 35 + dayOfWeekNum; i++) {
+    if (i < dayOfWeekNum) {
+      calendarDates.innerHTML += `<li></li>`;
+    } else if (dateNum < numberOfDays) {
+      dateNum++;
+      calendarDates.innerHTML += `<li><p class="selectable" data-date="${year}_${renderMonth}_${dateNum}_${dayOfWeekNum}">${dateNum}</p></li>`;
+      if (dayOfWeekNum < 6) {
+        dayOfWeekNum++;
+      } else {
+        dayOfWeekNum = 0;
+      }
+    }
+  }
+}
+
+function renderWeather() {
+  getUserData();
+  const WeatherArr = [honshu, hokkaido, okinawa];
+  let locationObj = userLocation.dataset.location;
+  let arrNum;
+
+  for (let i = 0; i < WeatherArr.length; i++) {
+    if (locationObj === "honshu") {
+      arrNum = 0;
+    } else if (locationObj === "hokkaido") {
+      arrNum = 1;
+    } else if (locationObj === "okinawa") {
+      arrNum = 2;
+    }
+  }
+
+  let currentWeatherArr = WeatherArr[arrNum][month][date - 1];
+  let weatherId = currentWeatherArr.weather;
+
+  let tempIcon;
+  if (currentWeatherArr.temp < 4) {
+    tempIcon = "cold";
+  } else if (currentWeatherArr.temp < 30) {
+    tempIcon = "normal";
+  } else {
+    tempIcon = "hot";
+  }
+
+  weatherResult.innerHTML = `
+      <p class="location">${userLocation.innerHTML}</p>
+      <h2>${year}年<br>${userDate.innerHTML}の天気</h2>
+      <div class="weather-icon">
+          <img src="./img/weather-icon-${weatherId}.jpg" alt="weather-image">
+      </div>
+      <p class="weather-text">
+          <span class="ja">${weatherDataYText[weatherId]}</span>
+      </p>
+      <div class="temp-wrap">
+          <div class="temp-image">
+              <img src="./img/weather-icon-temp-${tempIcon}.png" alt="temp-image-icon">
+          </div>
+          <div class="temp-text">
+              <div class="main-temp">気温:${currentWeatherArr.temp}℃</div>
+              <div class="min-max-temp">
+                  <span class="min-temp">最低気温:${currentWeatherArr.temp_min}℃</span>
+                  <span class="max-temp">最高気温:${currentWeatherArr.temp_max}℃</span>
+              </div>
+          </div>
+      </div>
+  `;
+}
+
+function getUserData() {
+  userLocation = currentLocation.children[0];
+  userDate = currentDate.children[0];
+}
+
+function subtractionMonth() {
   initializedDateHtml();
   if (month === 0) {
     month = 11;
@@ -40,8 +188,9 @@ prevMonth.addEventListener("click", function () {
   }
   month--;
   updateMonth(month);
-});
-nextMonth.addEventListener("click", function () {
+}
+
+function additionMonth() {
   initializedDateHtml();
   if (month === 11) {
     month = 0;
@@ -50,14 +199,13 @@ nextMonth.addEventListener("click", function () {
   }
   month++;
   updateMonth(month);
-});
+}
 
 function updateMonth(month) {
   dayOfWeekFirst = new Date(year, month, 1);
   numberOfDays = new Date(year, month + 1, 0).getDate();
   DOW = new Date(year, month, date).getDay();
   dayOfWeek = dayOfWeekStr[DOW];
-  console.log(numberOfDays);
   render();
 }
 
@@ -65,26 +213,29 @@ function initializedDateHtml() {
   calendarDates.innerHTML = "";
 }
 
-function render() {
-  let dateNum = 0;
-  let dayOfWeekNum = dayOfWeekFirst.getDay();
-  renderMonth = month + 1;
-  monthNam.innerHTML = renderMonth;
-  currentDate.innerHTML = `<p>${renderMonth}/${date}(${dayOfWeek})</p>`;
-
-  for (let i = 0; i <= 35 + dayOfWeekNum; i++) {
-    
-    if (i < dayOfWeekNum) {
-      
-      calendarDates.innerHTML += `<li></li>`;
-    } else if (dateNum < numberOfDays) {
-      dateNum++;
-      calendarDates.innerHTML += `<li class="selectable" data-date="${year}_${renderMonth}_${dateNum}_${dayOfWeekNum}">${dateNum}</li>`;
-      if (dayOfWeekNum < 6) {
-        dayOfWeekNum++;
-      } else {
-        dayOfWeekNum = 0;
-      }
-    }
+function AccordionHandler(tag, elem) {
+  if (tag.className === "active") {
+    elem.style.height = 0;
+    tag.classList.remove("active");
+  } else {
+    elem.style.height = elem.scrollHeight + "px";
+    tag.classList.add("active");
   }
+}
+
+function selectedLocation(e) {
+  const isClick = e.target.className === "otherLocation";
+  if (isClick) {
+    currentLocation.innerHTML = e.target.outerHTML;
+  }
+}
+
+function selectedDate(e) {
+  const isClick = e.target.className === "selectable";
+  if (!isClick) return;
+  const dateInfoArray = e.target.dataset.date.split("_");
+  renderMonth = parseFloat(dateInfoArray[1]);
+  date = parseFloat(dateInfoArray[2]);
+  dayOfWeek = dayOfWeekStr[parseFloat(dateInfoArray[3])];
+  currentDate.innerHTML = `<p data-date="${year}_${renderMonth}_${date}">${renderMonth}/${date}(${dayOfWeek})</p>`;
 }
